0bf4d7d6b90a7fce87f1dd1493dea6d778c3cf23
quang.phan <ptquang2000@gmail.com>
2026-08-15 22:33:47 +0000

    fix formatting

--- win32.cpp
+++ win32.cpp
@@ -26,32 +26,26 @@ enum class winspace_err : u64
 };
 
 template<winspace_err Error, typename Func, typename... Args>
-internal
-	auto
+internal auto
 CallWithError(Func&& func, Args&&... args)
-	-> std::expected<std::invoke_result_t<Func, Args...>, winspace_err>
+-> std::expected<std::invoke_result_t<Func, Args...>, winspace_err>
 {
 	using result_type = std::invoke_result_t<Func, Args...>;
 	if constexpr (Error == winspace_err::LastError)
 	{
 		if (auto r = std::invoke(
-					func,
-					std::forward<Args>(args)...
-					); r != nullptr)
+			func,
+			std::forward<Args>(args)...
+		); r != nullptr)
 		{
 			return r;
 		}
 		auto ec = GetLastError();
 		LPSTR buffer = nullptr;
 		if (auto bufSize = FormatMessageA(
-					FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM,
-					nullptr,
-					0,
-					0,
-					buffer,
-					0,
-					nullptr
-					); bufSize != 0)
+			FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_SYSTEM,
+			nullptr, 0, 0, buffer, 0, nullptr
+		); bufSize != 0)
 		{
 			OutputDebugStringA(buffer);
 			LocalFree(buffer);
@@ -64,14 +58,9 @@ CallWithError(Func&& func, Args&&... args)
 	}
 }
 
-internal
-LRESULT
-CALLBACK
-LowLevelKeyboardProc(
-		int    nCode,
-		WPARAM wParam,
-		LPARAM lParam
-		){
+internal LRESULT CALLBACK
+LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
 	auto vkCode = static_cast<int>(wParam);
 	switch (vkCode)
 	{
@@ -85,14 +74,12 @@ LowLevelKeyboardProc(
 	return CallNextHookEx(nullptr, nCode, wParam, lParam);
 }
 
-int 
-APIENTRY 
-WinMain(
-		HINSTANCE hPrevInstance,
-		HINSTANCE hInstance,
-		LPSTR     lpCmdLine,
-		int       nShowCmd
-       ){
+int APIENTRY
+WinMain(HINSTANCE hPrevInstance,
+	HINSTANCE hInstance,
+	LPSTR lpCmdLine,
+	int nShowCmd)
+{
 
 	auto running = g_running.get_token();
 
