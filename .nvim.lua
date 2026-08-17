108eb0efb3184c7b832a72008535841a8502e084
quang.phan <ptquang2000@gmail.com>
2026-08-17 13:13:29 +0700

    upd .nvim.lua

--- .nvim.lua
+++ .nvim.lua
@@ -1,9 +1,9 @@
 vim.keymap.set("n", "<leader>m", function()
-	vim.cmd("rightbelow vsplit")
-	if vim.fn.has("win32") == 1 then
-		vim.cmd.term("build.ps1")
-	else
-		vim.cmd.term("sh ./build.sh")
-	end
-	vim.cmd.stopinsert()
+  vim.cmd("rightbelow vsplit")
+  if vim.fn.has("win32") == 1 then
+    vim.cmd.term("powershell.exe -command ./build.ps1")
+  else
+    vim.cmd.term("sh ./build.sh")
+  end
+  vim.cmd.stopinsert()
 end, { desc = "Build winspace" })
