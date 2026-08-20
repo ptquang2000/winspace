1e2ac78d1db8fa4f34d9aa146f8bcd459c006c8f
quang.phan <ptquang2000@gmail.com>
2026-08-20 08:46:49 +0700

    clean keyboard events

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 export PATH=~/msvc/opt/msvc/bin/x64:$PATH
 mkdir -p build
 pushd build> /dev/null
-cl.exe -WX -W4 -wd4100 -Gm- -nologo -MT -Zi -Od -FC -std:c++latest ../win32.cpp User32.lib -Fe:winspace.exe
+cl.exe -WX -W4 -wd4100 -wd4189 -Gm- -nologo -MT -Zi -Od -FC -std:c++latest ../win32.cpp User32.lib -Fe:winspace.exe
 popd> /dev/null
--- win32.cpp
+++ win32.cpp
@@ -1,9 +1,11 @@
 #include <windows.h>
 
+#include <array>
 #include <deque>
 #include <expected>
 #include <format>
 #include <functional>
+#include <ranges>
 #include <stop_token>
 #include <string>
 
@@ -21,29 +23,35 @@
 #define u32		uint32_t
 #define u64		uint64_t
 
-struct KeyboardInputEvent
+namespace win32
+{
+struct keyboard_input
 {
     u32 time;
     u32 key;
     bool isPressed;
     bool isAltPressed;
 };
+}
 
-enum class winspace_err : u64
+namespace winspace
+{
+enum class err : u64
 {
     LastError,
 };
+}
 
 global std::stop_source g_running;
-global std::deque<KeyboardInputEvent> g_keyboardInputEvents;
+global std::deque<win32::keyboard_input> g_keyboardInputs;
 
-template<winspace_err Error, typename Func, typename... Args>
+template<winspace::err Error, typename Func, typename... Args>
 internal auto
 CallWithError(Func&& func, Args&&... args)
--> std::expected<std::invoke_result_t<Func, Args...>, winspace_err>
+-> std::expected<std::invoke_result_t<Func, Args...>, winspace::err>
 {
     using result_type = std::invoke_result_t<Func, Args...>;
-    if constexpr (Error == winspace_err::LastError)
+    if constexpr (Error == winspace::err::LastError)
     {
         if (auto r = std::invoke(
             func,
@@ -81,7 +89,7 @@ LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
         {
             auto kbDllHook = *reinterpret_cast<PKBDLLHOOKSTRUCT>(lParam);
             u32 flags = static_cast<u32>(kbDllHook.flags);
-            g_keyboardInputEvents.push_back(KeyboardInputEvent{ 
+            g_keyboardInputs.push_back(win32::keyboard_input{ 
                     .time = static_cast<u32>(kbDllHook.time),
                     .key = static_cast<u32>(kbDllHook.vkCode),
                     .isPressed = ((flags & LLKHF_UP) == 0),
@@ -96,13 +104,28 @@ LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
     return CallNextHookEx(nullptr, nCode, wParam, lParam);
 }
 
+namespace winspace
+{
+struct input_state
+{
+    int lastChangedTime;
+    bool wasDown;
+};
+
+template<std::size_t MaxKeyCode>
+struct controller_input
+{
+    std::array<input_state, MaxKeyCode> keys;
+};
+}
+
 int APIENTRY
 WinMain(HINSTANCE hPrevInstance,
         HINSTANCE hInstance,
         LPSTR lpCmdLine,
         int nShowCmd)
 {
-    auto hookExp = CallWithError<winspace_err::LastError>(
+    auto hookExp = CallWithError<winspace::err::LastError>(
         SetWindowsHookExA,
         WH_KEYBOARD_LL,
         LowLevelKeyboardProc,
@@ -114,8 +137,9 @@ WinMain(HINSTANCE hPrevInstance,
         return 1;
     }
 
-    auto hook = hookExp.value();
+    auto controllerInput = winspace::controller_input<VK_OEM_CLEAR>{};
     auto running = g_running.get_token();
+
     while (!running.stop_requested())
     {
         MSG msg{};
@@ -134,12 +158,21 @@ WinMain(HINSTANCE hPrevInstance,
 
             case WM_KEYBOARD:
             {
-                auto inputEvent = g_keyboardInputEvents.front();
-                OutputDebugStringA(std::format("{} is {}\n", 
-                        inputEvent.key,
-                        inputEvent.isPressed ? "pressed" : "released"
-                    ).c_str());
-                g_keyboardInputEvents.pop_front();
+                auto inputEvent = g_keyboardInputs.front();
+                g_keyboardInputs.pop_front();
+                auto &oldInputEvent = controllerInput.keys[inputEvent.key];
+                if (inputEvent.isPressed == oldInputEvent.wasDown)
+                {
+                    continue;
+                }
+                OutputDebugStringA(std::format("{} is {} at {}\n", 
+                            inputEvent.key,
+                            inputEvent.isPressed ? "pressed" : "released",
+                            inputEvent.time
+                            ).c_str());
+                
+                oldInputEvent.wasDown = inputEvent.isPressed;
+                oldInputEvent.lastChangedTime = inputEvent.time;
             } break;
 
             default:
@@ -149,6 +182,8 @@ WinMain(HINSTANCE hPrevInstance,
             } break;
         }
     }
+
+    auto hook = hookExp.value();
     UnhookWindowsHookEx(hook);
     return 0;
 }
