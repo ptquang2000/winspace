219d6691044bebd90f7cf466d8789bbc10cc9b5b
quang.phan <ptquang2000@gmail.com>
2026-08-23 06:12:55 +0700

    fix nvim script

--- .nvim.lua
+++ .nvim.lua
@@ -1,33 +1,22 @@
-local build_buf = nil
 local build_win = nil
 
 vim.keymap.set("n", "<leader>m", function()
-  local cmd = vim.fn.has("win32") == 1
-    and "powershell.exe -command ./build.ps1"
-    or "sh ./build.sh"
+	local cmd = vim.fn.has("win32") == 1 and { "powershell.exe", "-command", "./build.ps1" } or { "sh", "./build.sh" }
 
-  local cur_win = vim.api.nvim_get_current_win()
+	local old_buf = vim.fn.bufnr("winspace://build")
+	if old_buf ~= -1 then
+		vim.api.nvim_buf_delete(old_buf, { force = true })
+	end
 
-  -- reuse the split if it is still on screen, otherwise open one
-  if build_win and vim.api.nvim_win_is_valid(build_win) then
-    vim.api.nvim_set_current_win(build_win)
-  else
-    vim.cmd("rightbelow vsplit")
-    build_win = vim.api.nvim_get_current_win()
-  end
+	local new_buf = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_buf_call(new_buf, function()
+		vim.fn.jobstart(cmd, { term = true })
+	end)
+	vim.api.nvim_buf_set_name(new_buf, "winspace://build")
 
-  -- a terminal job cannot be restarted in place, so drop the old buffer
-  if build_buf and vim.api.nvim_buf_is_valid(build_buf) then
-    vim.api.nvim_buf_delete(build_buf, { force = true })
-  end
-
-  vim.cmd.term(cmd)
-  build_buf = vim.api.nvim_get_current_buf()
-  vim.api.nvim_buf_set_name(build_buf, "winspace://build")
-  vim.cmd.stopinsert()
-
-  -- hand focus back to wherever you were
-  if vim.api.nvim_win_is_valid(cur_win) then
-    vim.api.nvim_set_current_win(cur_win)
-  end
+	if not (build_win and vim.api.nvim_win_is_valid(build_win)) then
+		build_win = vim.api.nvim_open_win(new_buf, false, { split = "right", win = -1 })
+	else
+		vim.api.nvim_win_set_buf(build_win, new_buf)
+	end
 end, { desc = "Build winspace" })
