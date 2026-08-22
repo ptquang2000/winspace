fecf20824b2eab0068e6d52e83bb4216d2b9e7ce
quang.phan <ptquang2000@gmail.com>
2026-08-22 13:44:52 +0700

    no cross platform

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 new-item -itemtype directory -force -path .\build  > $null
 push-location .\build
-cl.exe -WX -W4 -wd4100 -wd4189 -Gm- -nologo -MT -Zi -Od -FC -std:c++latest ../win32.cpp User32.lib -Fe:winspace.exe
+cl.exe -WX -W4 -wd4100 -wd4189 -Gm- -nologo -MT -Zi -Od -FC -std:c++latest ../win32_winspace.cpp User32.lib -Fe:winspace.exe
 pop-location
--- win32_winspace.cpp
+++ win32_winspace.cpp
@@ -1,7 +1,26 @@
 #include <windows.h>
 
-#include "winspace.h"
-#include "winspace.cpp"
+#include <array>
+#include <deque>
+#include <expected>
+#include <format>
+#include <functional>
+#include <ranges>
+#include <stop_token>
+#include <string>
+#include <thread>
+
+#define internal	static
+#define global		static
+
+#define i8		int8_t
+#define i6		int16_t
+#define i2		int32_t
+#define i4		int64_t
+#define u8		uint8_t
+#define u16		uint16_t
+#define u32		uint32_t
+#define u64		uint64_t
 
 #define WM_KEYBOARD WM_USER
 
