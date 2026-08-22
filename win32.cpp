a96270670f036754fb0fc6fe04e578a5990c1933
quang.phan <ptquang2000@gmail.com>
2026-08-22 07:54:02 +0700

    add a dedicated keyboard thread

--- win32.cpp
+++ win32.cpp
@@ -1,57 +1,65 @@
 #include <windows.h>
 
-#include <array>
-#include <deque>
-#include <expected>
-#include <format>
-#include <functional>
-#include <ranges>
-#include <stop_token>
-#include <string>
+#include "winspace.h"
+#include "winspace.cpp"
 
 #define WM_KEYBOARD WM_USER
 
-#define internal	static
-#define global		static
-
-#define i8		int8_t
-#define i6		int16_t
-#define i2		int32_t
-#define i4		int64_t
-#define u8		uint8_t
-#define u16		uint16_t
-#define u32		uint32_t
-#define u64		uint64_t
+global DWORD g_mainThreadId;
 
 namespace win32
 {
-struct keyboard_input
+enum class error : u64
+{
+    LastError,
+    NullHandle,
+};
+
+namespace keyboard
+{
+struct input
 {
     u32 time;
     u32 key;
     bool isPressed;
     bool isAltPressed;
 };
-}
+global std::deque<input> g_keyboardInputs;
 
-namespace winspace
+internal LRESULT CALLBACK
+LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-enum class err : u64
-{
-    LastError,
-    NullHandle,
-};
+    switch (wParam)
+    {
+        case WM_SYSKEYDOWN: 
+        case WM_SYSKEYUP:
+        case WM_KEYDOWN:
+        case WM_KEYUP:
+        {
+            auto kbDllHook = *reinterpret_cast<PKBDLLHOOKSTRUCT>(lParam);
+            u32 flags = static_cast<u32>(kbDllHook.flags);
+            g_keyboardInputs.push_back(input{ 
+                    .time = static_cast<u32>(kbDllHook.time),
+                    .key = static_cast<u32>(kbDllHook.vkCode),
+                    .isPressed = ((flags & LLKHF_UP) == 0),
+                    .isAltPressed = ((flags & LLKHF_ALTDOWN) != 0),
+            });
+            PostThreadMessageA(g_mainThreadId, WM_KEYBOARD, 0, 0);
+        } break;
+        default:
+        {
+        } break;
+    }
+    return CallNextHookEx(nullptr, nCode, wParam, lParam);
 }
+} // keyboard_lowlevel
 
-global std::stop_source g_running;
-global std::deque<win32::keyboard_input> g_keyboardInputs;
-
-template<winspace::err Error, typename Func, typename... Args>
+template<error Error, typename Func, typename... Args>
 internal auto
 CallWithError(Func&& func, Args&&... args)
--> std::expected<std::invoke_result_t<Func, Args...>, winspace::err>
+-> std::expected<std::invoke_result_t<Func, Args...>, win32::error>
 {
-    if constexpr (Error == winspace::err::LastError)
+    if constexpr (Error == win32::error::LastError)
     {
         if (auto r = std::invoke(
             func,
@@ -71,7 +79,7 @@ CallWithError(Func&& func, Args&&... args)
         }
         return std::unexpected(Error);
     }
-    else if constexpr (Error == winspace::err::NullHandle)
+    else if constexpr (Error == win32::error::NullHandle)
     {
         if (auto handle = std::invoke(
             func,
@@ -86,36 +94,38 @@ CallWithError(Func&& func, Args&&... args)
     {
         static_assert(0, "unhandle error");
     }
-}       
-
-internal LRESULT CALLBACK
-LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
-{
-    switch (wParam)
-    {
-        case WM_SYSKEYDOWN: 
-        case WM_SYSKEYUP:
-        case WM_KEYDOWN:
-        case WM_KEYUP:
-        {
-            auto kbDllHook = *reinterpret_cast<PKBDLLHOOKSTRUCT>(lParam);
-            u32 flags = static_cast<u32>(kbDllHook.flags);
-            g_keyboardInputs.push_back(win32::keyboard_input{ 
-                    .time = static_cast<u32>(kbDllHook.time),
-                    .key = static_cast<u32>(kbDllHook.vkCode),
-                    .isPressed = ((flags & LLKHF_UP) == 0),
-                    .isAltPressed = ((flags & LLKHF_ALTDOWN) != 0),
-            });
-            PostMessageA(nullptr, WM_KEYBOARD, 0, 0);
-        } break;
-        default:
-        {
-        } break;
-    }
-    return CallNextHookEx(nullptr, nCode, wParam, lParam);
 }
 
-VOID 
+struct controller_input
+{
+    struct input_state
+    {
+        int lastChangedTime;
+        bool wasDown;
+    };
+    std::array<input_state, VK_OEM_CLEAR> vkeys;
+};
+
+void
+ProcessKeyboardInput(keyboard::input keyboardInput,
+                    controller_input& controllerInput)
+{
+    auto &oldInput = controllerInput.vkeys[keyboardInput.key];
+    if (keyboardInput.isPressed == oldInput.wasDown)
+    {
+        return;
+    }
+    OutputDebugStringA(std::format("{} is {} at {}\n", 
+                keyboardInput.key,
+                keyboardInput.isPressed ? "pressed" : "released",
+                keyboardInput.time
+                ).c_str());
+    oldInput.wasDown = keyboardInput.isPressed;
+    oldInput.lastChangedTime = keyboardInput.time;
+}
+} // win32
+
+internal VOID 
 Wineventproc(HWINEVENTHOOK hWinEventHook, DWORD event, HWND hwnd, LONG idObject,
             LONG idChild, DWORD idEventThread, DWORD dwmsEventTime)
 {
@@ -438,20 +448,6 @@ Wineventproc(HWINEVENTHOOK hWinEventHook, DWORD event, HWND hwnd, LONG idObject,
     }
 }
 
-namespace winspace
-{
-struct input_state
-{
-    int lastChangedTime;
-    bool wasDown;
-};
-
-template<std::size_t MaxKeyCode>
-struct controller_input
-{
-    std::array<input_state, MaxKeyCode> keys;
-};
-}
 
 int APIENTRY
 WinMain(HINSTANCE hPrevInstance,
@@ -459,7 +455,9 @@ WinMain(HINSTANCE hPrevInstance,
         LPSTR lpCmdLine,
         int nShowCmd)
 {
-    auto winEventHookExp = CallWithError<winspace::err::NullHandle>(
+    g_mainThreadId = GetCurrentThreadId();
+
+    auto winEventHookExp = win32::CallWithError<win32::error::NullHandle>(
         SetWinEventHook, 
         EVENT_MIN, EVENT_MAX, nullptr, Wineventproc, 0, 0,
         WINEVENT_OUTOFCONTEXT | WINEVENT_SKIPOWNPROCESS);
@@ -469,54 +467,52 @@ WinMain(HINSTANCE hPrevInstance,
         return 1;
     }
 
-    auto keyboardHookExp = CallWithError<winspace::err::LastError>(
-        SetWindowsHookExA,
-        WH_KEYBOARD_LL,
-        LowLevelKeyboardProc,
-        GetModuleHandleA(nullptr),
-        0);
-    if (!keyboardHookExp.has_value())
-    {
-        OutputDebugStringA("Failed to hook SetWindowsHookExA:WH_KEYBOARD_LL");
-        return 1;
-    }
+    std::jthread keyboardHookThread([](std::stop_token running){
+        auto keyboardHookExp = win32::CallWithError<win32::error::LastError>(
+                SetWindowsHookExA,
+                WH_KEYBOARD_LL, win32::keyboard::LowLevelKeyboardProc,
+                GetModuleHandleA(nullptr), 0);
+        if (!keyboardHookExp.has_value())
+        {
+            OutputDebugStringA(
+                    "Failed to hook SetWindowsHookExA:WH_KEYBOARD_LL");
+            return;
+        }
 
-    auto controllerInput = winspace::controller_input<VK_OEM_CLEAR>{};
-    auto running = g_running.get_token();
+        while (!running.stop_requested())
+        {
+            MSG msg{};
+            if (GetMessageA(&msg, 0, 0, 0) < 0)
+            {
+                continue;
+            }
+            TranslateMessage(&msg);
+            DispatchMessageA(&msg);
+        }
+        UnhookWindowsHookEx(keyboardHookExp.value());
+    });
 
-    while (!running.stop_requested())
+    auto controllerInput = win32::controller_input{};
+    bool running = true;
+    while (running)
     {
         MSG msg{};
-        if (GetMessageA(&msg, 0, 0, 0) < 0)
+        if (GetMessageA(&msg, 0, 0, 0) <= 0)
         {
-            g_running.request_stop();
+            UnhookWinEvent(winEventHookExp.value());
+            keyboardHookThread.request_stop();
+            running = false;
             continue;
         }
 
         switch(msg.message)
         {
-            case WM_QUIT:
-            {
-                g_running.request_stop();
-            } break;
-
+            using namespace win32;
             case WM_KEYBOARD:
             {
-                auto inputEvent = g_keyboardInputs.front();
-                g_keyboardInputs.pop_front();
-                auto &oldInputEvent = controllerInput.keys[inputEvent.key];
-                if (inputEvent.isPressed == oldInputEvent.wasDown)
-                {
-                    continue;
-                }
-                OutputDebugStringA(std::format("{} is {} at {}\n", 
-                            inputEvent.key,
-                            inputEvent.isPressed ? "pressed" : "released",
-                            inputEvent.time
-                            ).c_str());
-                
-                oldInputEvent.wasDown = inputEvent.isPressed;
-                oldInputEvent.lastChangedTime = inputEvent.time;
+                ProcessKeyboardInput(
+                        keyboard::g_keyboardInputs.front(), controllerInput);
+                keyboard::g_keyboardInputs.pop_front();
             } break;
 
             default:
@@ -526,8 +522,5 @@ WinMain(HINSTANCE hPrevInstance,
             } break;
         }
     }
-
-    UnhookWindowsHookEx(keyboardHookExp.value());
-    UnhookWinEvent(winEventHookExp.value());
     return 0;
 }
---
+++ winspace.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <array>
+#include <deque>
+#include <expected>
+#include <format>
+#include <functional>
+#include <ranges>
+#include <stop_token>
+#include <string>
+#include <thread>
+
+#define internal	static
+#define global		static
+
+#define i8		int8_t
+#define i6		int16_t
+#define i2		int32_t
+#define i4		int64_t
+#define u8		uint8_t
+#define u16		uint16_t
+#define u32		uint32_t
+#define u64		uint64_t
+
+namespace winspace
+{
+struct command_inputs
+{
+};
+} // winspace
+
